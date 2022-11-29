28acf5c92086ef4382e343c4772e09ea0ca0ad6d
sergega-fan <sergei_nikitin00@mail.ru>
2022-11-29 19:27:40 +0300

    Header done

--- src/hoc/Header.tsx
+++ src/hoc/Header.tsx
@@ -1,20 +1,29 @@
-import React, {ReactNode} from 'react';
+import React, { ReactNode } from "react";
+import ArrowLeft from "../components/common/svg/ArrowLeft";
+import Logo from "../components/common/svg/Logo";
 
 interface IHeader {
-	children: ReactNode
+  children: ReactNode;
 }
 
-const Header = ({children}: IHeader) => {
-	return (
-		<>
-			<header className={"header"}>
-				<div className="container">
-					header
-				</div>
-			</header>
-			{children}
-		</>
-	);
+const Header = ({ children }: IHeader) => {
+  return (
+    <>
+      <header className={"header"}>
+        <div className="container">
+          <div className="headerInner">
+            <a className={"BackBtn"} href="#">
+              <ArrowLeft />
+              <span>На главную</span>
+            </a>
+
+            <Logo />
+          </div>
+        </div>
+      </header>
+      {children}
+    </>
+  );
 };
 
 export default Header;
