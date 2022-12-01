6871cbf029ca0d2de036cec29a595614181e32cd
sergega-fan <sergei_nikitin00@mail.ru>
2022-12-01 23:37:30 +0300

    done selects

--- src/components/shared/MainScreen/MainScreenModal.tsx
+++ src/components/shared/MainScreen/MainScreenModal.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import tabs from "../../../data/MainScreenData";
 import Close from "../../common/svg/Close";
-import Hands from "../../common/svg/MainPopup/Hands";
+import Select from "react-select";
 
 interface MainScreenModalI {
   show: boolean;
@@ -9,7 +9,30 @@ interface MainScreenModalI {
   type: string;
 }
 
+const isPersonOptions = [
+  { value: "man", label: "Мужской" },
+  { value: "woman", label: "Женский" },
+];
+
+const maleOptions = [
+  { value: "man", label: "Мужской" },
+  { value: "woman", label: "Женский" },
+];
+
 const MainScreenModal = ({ show, handleClose, type }: MainScreenModalI) => {
+  const [name, setName] = useState<string>("");
+  const editName = useCallback((e: any) => {
+    setName(e.target.value);
+  }, []);
+  const [date, setDate] = useState<string>("");
+  const editDate = useCallback((e: any) => {
+    setDate(e.target.value);
+  }, []);
+
+  const calcFunction = () => {
+    console.log("calc");
+  };
+
   return (
     <div className={`CalculationModal ${show ? "active" : ""}`}>
       <div className="CalculationModalInner">
@@ -18,8 +41,61 @@ const MainScreenModal = ({ show, handleClose, type }: MainScreenModalI) => {
         <p>{tabs[type].popupText}</p>
         <div className="formWrapper">
           <div className="form">
-            <input placeholder={"Имя*"} type="text" />
-            <input type="text" />
+            {tabs[type].id == 1 && (
+              <>
+                <input
+                  value={name}
+                  onChange={editName}
+                  placeholder={"Имя*"}
+                  type="text"
+                />
+                <input value={date} onChange={editDate} type="date" />
+                <div className="selectWrapper">
+                  <Select
+                    classNames={{
+                      container: (state) => "selectContainer",
+                      valueContainer: (state) => "selectValueContainer",
+                      input: (state) => "selectInput",
+                      control: (state) =>
+                        state.isFocused ? "selectData" : "selectData",
+                    }}
+                    options={isPersonOptions}
+                  />
+                </div>
+                <div className="selectWrapper">
+                  <Select
+                    classNames={{
+                      container: (state) => "selectContainer",
+                      control: (state) => "selectData",
+                    }}
+                    options={maleOptions}
+                  />
+                </div>
+              </>
+            )}
+            {tabs[type].id == 2 && (
+              <>
+                <input
+                  value={name}
+                  onChange={editName}
+                  placeholder={"Имя*"}
+                  type="text"
+                />
+                <input value={date} onChange={editDate} type="date" />
+              </>
+            )}
+            {tabs[type].id == 3 && (
+              <>
+                <input
+                  value={name}
+                  onChange={editName}
+                  placeholder={"Имя*"}
+                  type="text"
+                />
+                <input value={date} onChange={editDate} type="date" />
+              </>
+            )}
+            <button onClick={calcFunction}>Рассчитать</button>
           </div>
           <div className="imageWrapper">{tabs[type].popupImg()}</div>
         </div>
