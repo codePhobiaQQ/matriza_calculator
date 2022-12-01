5d2b5b078a5def79302b89f1c1fdfc3a0403aded
sergega-fan <sergei_nikitin00@mail.ru>
2022-12-01 23:05:49 +0300

    image in popup

--- src/components/shared/MainScreen/MainScreenModal.tsx
+++ src/components/shared/MainScreen/MainScreenModal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import tabs from "../../../data/MainScreenData";
 import Close from "../../common/svg/Close";
+import Hands from "../../common/svg/MainPopup/Hands";
 
 interface MainScreenModalI {
   show: boolean;
@@ -14,6 +15,14 @@ const MainScreenModal = ({ show, handleClose, type }: MainScreenModalI) => {
       <div className="CalculationModalInner">
         <Close clickHandler={handleClose} />
         <h2>{tabs[type].popupTitle}</h2>
+        <p>{tabs[type].popupText}</p>
+        <div className="formWrapper">
+          <div className="form">
+            <input placeholder={"Имя*"} type="text" />
+            <input type="text" />
+          </div>
+          <div className="imageWrapper">{tabs[type].popupImg()}</div>
+        </div>
       </div>
     </div>
   );
