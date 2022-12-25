b4e92793c4d8a0c2228755be174fb8cd0e84b7a1
sergega-fan <sergei_nikitin00@mail.ru>
2022-12-25 17:40:33 +0300

    Done matrix periods to 60 years

--- src/components/shared/CalcMatrix/Matrix.tsx
+++ src/components/shared/CalcMatrix/Matrix.tsx
@@ -188,123 +188,173 @@ const Matrix = () => {
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-b8"
               data-personal-calculation-position="b8"
-            ></div>
+            >
+              {matrixData?.age25_1}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-b7"
               data-personal-calculation-position="b7"
-            ></div>
+            >
+              {matrixData?.age25_2}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-k2"
               data-personal-calculation-position="k2"
-            ></div>
+            >
+              {matrixData?.age25_3}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-k1 -bold"
               data-personal-calculation-position="k1"
-            ></div>
+            >
+              {matrixData?.age25}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-k4"
               data-personal-calculation-position="k4"
-            ></div>
+            >{matrixData?.age30_1}</div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-k3"
               data-personal-calculation-position="k3"
-            ></div>
+            >{matrixData?.age30_2}</div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-k5"
               data-personal-calculation-position="k5"
-            ></div>
+            >{matrixData?.age30_3}</div>
 
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-k8"
               data-personal-calculation-position="k8"
-            ></div>
+            >
+              {matrixData?.age35_1}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-k7"
               data-personal-calculation-position="k7"
-            ></div>
+            >
+              {matrixData?.age35_2}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-k6"
               data-personal-calculation-position="k6"
-            ></div>
+            >
+              {matrixData?.age35_3}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-c4 -bold"
               data-personal-calculation-position="c4"
-            ></div>
+            >
+              {matrixData?.age35}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-c6"
               data-personal-calculation-position="c6"
-            ></div>
+            >
+              {matrixData?.age40_1}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-c5"
               data-personal-calculation-position="c5"
-            ></div>
+            >
+              {matrixData?.age40_2}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-c7"
               data-personal-calculation-position="c7"
-            ></div>
+            >
+              {matrixData?.age40_3}
+            </div>
 
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-y3"
               data-personal-calculation-position="y3"
-            ></div>
+            >
+              {matrixData?.age45_1}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-y2"
               data-personal-calculation-position="y2"
-            ></div>
+            >
+              {matrixData?.age45_2}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-y4"
               data-personal-calculation-position="y4"
-            ></div>
+            >
+              {matrixData?.age45_3}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-y1 -bold"
               data-personal-calculation-position="y1"
-            ></div>
+            >{matrixData?.age45}</div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-y6"
               data-personal-calculation-position="y6"
-            ></div>
+            >
+              {matrixData?.age50_1}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-y5"
               data-personal-calculation-position="y5"
-            ></div>
+            >
+              {matrixData?.age50_2}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-y7"
               data-personal-calculation-position="y7"
-            ></div>
+            >
+              {matrixData?.age50_3}
+            </div>
 
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-d6"
               data-personal-calculation-position="d6"
-            ></div>
+            >
+              {matrixData?.age55_1}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-y8"
               data-personal-calculation-position="y8"
-            ></div>
+            >
+              {matrixData?.age55_2}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-d5"
               data-personal-calculation-position="d5"
-            ></div>
+            >
+              {matrixData?.age55_3}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-d4 -bold"
               data-personal-calculation-position="d4"
-            ></div>
+            >
+              {matrixData?.age55}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-d8"
               data-personal-calculation-position="d8"
-            ></div>
+            >
+              {matrixData?.age60_1}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-d7"
               data-personal-calculation-position="d7"
-            ></div>
+            >
+              {matrixData?.age60_2}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-d9"
               data-personal-calculation-position="d9"
-            ></div>
+            >
+              {matrixData?.age60_3}
+            </div>
 
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-g3"
               data-personal-calculation-position="g3"
-            ></div>
+            >
+              {/*{matrixData?.age65_1}*/}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-g2"
               data-personal-calculation-position="g2"
@@ -362,60 +412,80 @@ const Matrix = () => {
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-a5"
               data-personal-calculation-position="a5"
-            ></div>
+            >{matrixData?.age5_1}</div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-a4"
               data-personal-calculation-position="a4"
-            ></div>
+            >{matrixData?.age5_2}</div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-a6"
               data-personal-calculation-position="a6"
-            ></div>
+            >{matrixData?.age5_3}</div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-f1 -bold"
               data-personal-calculation-position="f1"
-            ></div>
+            >
+              {matrixData?.age5}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-f3"
               data-personal-calculation-position="f3"
-            ></div>
+            >
+              {matrixData?.age10_1}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-f2"
               data-personal-calculation-position="f2"
-            ></div>
+            >
+              {matrixData?.age10_2}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-f4"
               data-personal-calculation-position="f4"
-            ></div>
+            >
+              {matrixData?.age10_3}
+            </div>
 
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-f7"
               data-personal-calculation-position="f7"
-            ></div>
+            >
+              {matrixData?.age15_1}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-f6"
               data-personal-calculation-position="f6"
-            ></div>
+            >
+              {matrixData?.age15_2}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-f8"
               data-personal-calculation-position="f8"
-            ></div>
+            >
+              {matrixData?.age15_3}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-f5 -bold"
               data-personal-calculation-position="f5"
-            ></div>
+            >
+              {matrixData?.age15}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-b5"
               data-personal-calculation-position="b5"
-            ></div>
+            >
+              {matrixData?.age20_1}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-b4"
               data-personal-calculation-position="b4"
-            ></div>
+            >
+              {matrixData?.age20_2}
+            </div>
             <div
               className="matrix-diagram__number js-personal-calculation-item -position-b6"
               data-personal-calculation-position="b6"
-            ></div>
+            >{matrixData?.age20_3}</div>
           </div>
         </div>
       </div>
