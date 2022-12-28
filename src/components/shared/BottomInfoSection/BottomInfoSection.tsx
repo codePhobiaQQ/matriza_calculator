430e2d3b568c5e55adaa7d486ca9450e2b956eec
sergega-fan <sergei_nikitin00@mail.ru>
2022-12-28 16:12:44 +0300

    Bottom info done

--- src/components/shared/BottomInfoSection/BottomInfoSection.tsx
+++ src/components/shared/BottomInfoSection/BottomInfoSection.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {useAppSelector} from "../../../hooks/redux";
+import fixTwentyTwo from "../../../utils/fixTwentyTwo";
 
 interface BottomInfoSectionI {
     classing?: string;
 }
 
 const BottomInfoSection = ({classing = ""}: BottomInfoSectionI) => {
+  const matrixData = useAppSelector(state => state.app.matrixData)
+
     return (
         <div className={`${classing} BottomInfoSection`}>
             <div className="BottomInfoSection_left BottomInfoSection_side">
@@ -13,15 +17,15 @@ const BottomInfoSection = ({classing = ""}: BottomInfoSectionI) => {
         <div className="BottomInfoSection_resultLines">
             <div className="BottomInfoSection_resultLine">
                 <span className={"BottomInfoSection_resultLine_title"}>Небо</span>
-                <span className={"BottomInfoSection_resultLine_number"}>11</span>
+                <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.h}</span>
             </div>
             <div className="BottomInfoSection_resultLine BottomInfoSection_resultLine_center">
                 <span className={"BottomInfoSection_resultLine_title"}></span>
-                <span className={"BottomInfoSection_resultLine_number"}>11</span>
+                <span className={"BottomInfoSection_resultLine_number"}>{fixTwentyTwo(Number(matrixData?.j) + Number(matrixData?.h))}</span>
             </div>
             <div className="BottomInfoSection_resultLine">
                 <span className={"BottomInfoSection_resultLine_title"}>Земля</span>
-                <span className={"BottomInfoSection_resultLine_number"}>11</span>
+                <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.j}</span>
             </div>
         </div>
     </div>
@@ -33,15 +37,15 @@ const BottomInfoSection = ({classing = ""}: BottomInfoSectionI) => {
             <div className="BottomInfoSection_resultLines">
                 <div className="BottomInfoSection_resultLine">
                     <span className={"BottomInfoSection_resultLine_title"}>M</span>
-                    <span className={"BottomInfoSection_resultLine_number"}>11</span>
+                    <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.n}</span>
                 </div>
                 <div className="BottomInfoSection_resultLine BottomInfoSection_resultLine_center">
                     <span className={"BottomInfoSection_resultLine_title"}></span>
-                    <span className={"BottomInfoSection_resultLine_number"}>11</span>
+                    <span className={"BottomInfoSection_resultLine_number"}>{fixTwentyTwo(Number(matrixData?.n) + Number(matrixData?.t))}</span>
                 </div>
                 <div className="BottomInfoSection_resultLine">
                     <span className={"BottomInfoSection_resultLine_title"}>Ж</span>
-                    <span className={"BottomInfoSection_resultLine_number"}>11</span>
+                    <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.t}</span>
                 </div>
             </div>
         </div>
@@ -52,7 +56,7 @@ const BottomInfoSection = ({classing = ""}: BottomInfoSectionI) => {
                         </p>
                         <div className="BottomInfoSection_resultLines">
                             <div className="BottomInfoSection_resultLine">
-                                <span className={"BottomInfoSection_resultLine_number"}>15</span>
+                                <span className={"BottomInfoSection_resultLine_number"}>{fixTwentyTwo(fixTwentyTwo(Number(matrixData?.j) + Number(matrixData?.h)) + Number(matrixData?.n) + Number(matrixData?.t))}</span>
                             </div>
                         </div>
                     </div>
--- src/functions/onlineMatrixCalculation.tsx
+++ src/functions/onlineMatrixCalculation.tsx
@@ -103,6 +103,11 @@ export interface matrixData {
     age60_2: number;
     age60_3: number;
     age60: number;
+
+    h: number;
+    j: number;
+    n: number;
+    t: number;
 }
 
 const onlineMatrixCalculation = (date: string): matrixData => {
@@ -221,9 +226,15 @@ const onlineMatrixCalculation = (date: string): matrixData => {
     const age60_1 = fixTwentyTwo(age55 + age60_2)
     const age60_3 = fixTwentyTwo(age60_2 + age60)
 
+
+    const h = fixTwentyTwo(b + d)
+    const j = fixTwentyTwo(a + c)
+    const n = fixTwentyTwo(f + y)
+    const t = fixTwentyTwo(g + k)
+
     return {
         a, b, c, d, e, f, g, y, k, a1, b1, c1, d1, d2, x, t1, t2, a2, a3, b2, b3, c2, e1, e2, p1, p2, p3, p4, s1, s2, s3, s4, x1, x2,
-        age0, age5, age5_1, age5_2, age5_3, age10, age10_1, age10_2, age10_3, age15, age15_1, age15_2, age15_3, age20, age20_1, age20_2, age20_3, age25, age25_1, age25_2, age25_3, age30, age30_1, age30_2, age30_3, age35, age35_1, age35_2, age35_3, age40, age40_1, age40_2, age40_3, age45, age45_1, age45_2, age45_3, age50, age50_1, age50_2, age50_3, age55, age55_1, age55_2, age55_3, age60, age60_1, age60_2, age60_3, l, l1, l2, l3, l4, l5, l6
+        age0, age5, age5_1, age5_2, age5_3, age10, age10_1, age10_2, age10_3, age15, age15_1, age15_2, age15_3, age20, age20_1, age20_2, age20_3, age25, age25_1, age25_2, age25_3, age30, age30_1, age30_2, age30_3, age35, age35_1, age35_2, age35_3, age40, age40_1, age40_2, age40_3, age45, age45_1, age45_2, age45_3, age50, age50_1, age50_2, age50_3, age55, age55_1, age55_2, age55_3, age60, age60_1, age60_2, age60_3, l, l1, l2, l3, l4, l5, l6, h, j, n, t
     }
 };
 
