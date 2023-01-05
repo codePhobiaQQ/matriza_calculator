de5d7d70a0b250f544ad14fcc6f1ca8c361c2a96
sergega-fan <sergei_nikitin00@mail.ru>
2023-01-05 12:26:45 +0300

    Done bottom description

--- src/components/shared/ActionSection/ActionSection.tsx
+++ src/components/shared/ActionSection/ActionSection.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import {useAppSelector} from "../../../hooks/redux";
 
 interface ActionSectionI {
   classing?: string;
 }
 
 const ActionSection = ({classing = ""}: ActionSectionI) => {
+  const activeTab = useAppSelector(state => state.app.activeTab)
+
   return (
-    <div className={`ActionSection ${classing}`}>
-      <h3>Для получения подробного описания Вашей матрицы судьбы обратитесь к эксперту Школы BRESSEL </h3>
-      <button>Запись на консультацию</button>
-    </div>
+    <>
+      {activeTab == 1 &&
+      <div className={`ActionSection ${classing}`}>
+          <h3>Эксперты Школы BRESSEL владеют уникальными знаниями для расчета Матрицы года и если Вы хотите узнать, что принесёт лично Вам текущий или предстоящий год, напишите нам!</h3>
+          <button>Заказать</button>
+      </div>
+      }
+      {activeTab == 2 &&
+      <div className={`ActionSection ${classing}`}>
+          <h3>Для получения подробного описания Вашей матрицы судьбы обратитесь к эксперту Школы BRESSEL </h3>
+          <button>Запись на консультацию</button>
+      </div>
+      }
+    </>
   );
 };
 
