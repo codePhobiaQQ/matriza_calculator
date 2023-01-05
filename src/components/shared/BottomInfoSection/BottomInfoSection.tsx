91cd33752c2dd8f34b56d8b9af5f69c5f02a2ebe
sergega-fan <sergei_nikitin00@mail.ru>
2023-01-05 13:11:42 +0300

    Fixing text

--- src/components/shared/BottomInfoSection/BottomInfoSection.tsx
+++ src/components/shared/BottomInfoSection/BottomInfoSection.tsx
@@ -11,9 +11,9 @@ const titleData = {
     title1: "Личное",
     title2: "Социальное",
     title3: "Духовное",
-    description1: "1",
-    description2: "2",
-    description3: "3"
+    description1: "Соединение мужского и женского. Выстраивание взаимоотношений. Способности, навыки, умения.",
+    description2: "Социальная и родовая системы. Результаты и признания в социуме.",
+    description3: "Духовный зачет. Кто я для бога? Где божественное во мне?"
   },
   2: {
     title1: "Отношения",
@@ -22,14 +22,6 @@ const titleData = {
     description1: "Показывает, что партнёры должны проработать каждый для себя и чему должны научиться, чтобы быть вместе.",
     description2: " Определяет, какую социальную роль выполняет пара, какую задачу выполняет и какую энергию отдает своему роду. ",
     description3: "Это духовная составляющая пары, это то, на чем базируется пара (ресурс) и куда и к чему ей следует стремиться в своем развитии."
-  },
-  3: {
-    title1: "Поиск себя",
-    title2: "Социализация",
-    title3: "Духовная грамотность",
-    description1: "1",
-    description2: "2",
-    description3: "3"
   },
 }
 
@@ -40,55 +32,55 @@ const BottomInfoSection = ({classing = ""}: BottomInfoSectionI) => {
   return (
     <>
       {activeTab !== 3 && <div className={`${classing} BottomInfoSection`}>
-        <div className="BottomInfoSection_left BottomInfoSection_side">
-          <h3>{titleData[activeTab || 1].title1}</h3>
-          <p>{titleData[activeTab || 1].description2}</p>
-          <div className="BottomInfoSection_resultLines">
-            <div className="BottomInfoSection_resultLine">
-              <span className={"BottomInfoSection_resultLine_title"}>Небо</span>
-              <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.h}</span>
-            </div>
-            <div className="BottomInfoSection_resultLine BottomInfoSection_resultLine_center">
-              <span className={"BottomInfoSection_resultLine_title"}></span>
-              <span
-                className={"BottomInfoSection_resultLine_number"}>{fixTwentyTwo(Number(matrixData?.j) + Number(matrixData?.h))}</span>
-            </div>
-            <div className="BottomInfoSection_resultLine">
-              <span className={"BottomInfoSection_resultLine_title"}>Земля</span>
-              <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.j}</span>
-            </div>
+          <div className="BottomInfoSection_left BottomInfoSection_side">
+              <h3>{titleData[activeTab || 1].title1}</h3>
+              <p>{titleData[activeTab || 1].description2}</p>
+              <div className="BottomInfoSection_resultLines">
+                  <div className="BottomInfoSection_resultLine">
+                      <span className={"BottomInfoSection_resultLine_title"}>Небо</span>
+                      <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.h}</span>
+                  </div>
+                  <div className="BottomInfoSection_resultLine BottomInfoSection_resultLine_center">
+                      <span className={"BottomInfoSection_resultLine_title"}></span>
+                      <span
+                          className={"BottomInfoSection_resultLine_number"}>{fixTwentyTwo(Number(matrixData?.j) + Number(matrixData?.h))}</span>
+                  </div>
+                  <div className="BottomInfoSection_resultLine">
+                      <span className={"BottomInfoSection_resultLine_title"}>Земля</span>
+                      <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.j}</span>
+                  </div>
+              </div>
           </div>
-        </div>
-        <div className="BottomInfoSection_center BottomInfoSection_side">
-          <h3>{titleData[activeTab || 1].title2}</h3>
-          <p>{titleData[activeTab || 1].description2}</p>
-          <div className="BottomInfoSection_resultLines">
-            <div className="BottomInfoSection_resultLine">
-              <span className={"BottomInfoSection_resultLine_title"}>M</span>
-              <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.n}</span>
-            </div>
-            <div className="BottomInfoSection_resultLine BottomInfoSection_resultLine_center">
-              <span className={"BottomInfoSection_resultLine_title"}></span>
-              <span
-                className={"BottomInfoSection_resultLine_number"}>{fixTwentyTwo(Number(matrixData?.n) + Number(matrixData?.t))}</span>
-            </div>
-            <div className="BottomInfoSection_resultLine">
-              <span className={"BottomInfoSection_resultLine_title"}>Ж</span>
-              <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.t}</span>
-            </div>
+          <div className="BottomInfoSection_center BottomInfoSection_side">
+              <h3>{titleData[activeTab || 1].title2}</h3>
+              <p>{titleData[activeTab || 1].description2}</p>
+              <div className="BottomInfoSection_resultLines">
+                  <div className="BottomInfoSection_resultLine">
+                      <span className={"BottomInfoSection_resultLine_title"}>M</span>
+                      <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.n}</span>
+                  </div>
+                  <div className="BottomInfoSection_resultLine BottomInfoSection_resultLine_center">
+                      <span className={"BottomInfoSection_resultLine_title"}></span>
+                      <span
+                          className={"BottomInfoSection_resultLine_number"}>{fixTwentyTwo(Number(matrixData?.n) + Number(matrixData?.t))}</span>
+                  </div>
+                  <div className="BottomInfoSection_resultLine">
+                      <span className={"BottomInfoSection_resultLine_title"}>Ж</span>
+                      <span className={"BottomInfoSection_resultLine_number"}>{matrixData?.t}</span>
+                  </div>
+              </div>
           </div>
-        </div>
-        <div className="BottomInfoSection_right BottomInfoSection_side">
-          <h3>{titleData[activeTab || 1].title3}</h3>
-          <p>{titleData[activeTab || 1].description3}
-          </p>
-          <div className="BottomInfoSection_resultLines">
-            <div className="BottomInfoSection_resultLine">
+          <div className="BottomInfoSection_right BottomInfoSection_side">
+              <h3>{titleData[activeTab || 1].title3}</h3>
+              <p>{titleData[activeTab || 1].description3}
+              </p>
+              <div className="BottomInfoSection_resultLines">
+                  <div className="BottomInfoSection_resultLine">
             <span
-              className={"BottomInfoSection_resultLine_number"}>{fixTwentyTwo(fixTwentyTwo(Number(matrixData?.j) + Number(matrixData?.h)) + Number(matrixData?.n) + Number(matrixData?.t))}</span>
-            </div>
+                className={"BottomInfoSection_resultLine_number"}>{fixTwentyTwo(fixTwentyTwo(Number(matrixData?.j) + Number(matrixData?.h)) + Number(matrixData?.n) + Number(matrixData?.t))}</span>
+                  </div>
+              </div>
           </div>
-        </div>
       </div>}
     </>
   );
