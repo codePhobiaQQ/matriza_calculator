b0daf5f5f64ffb7561eebeddce8ee263246511ba
sergega-fan <sergei_nikitin00@mail.ru>
2023-01-12 20:16:24 +0300

    Done redirect link

--- src/components/shared/ActionSection/ActionSection.tsx
+++ src/components/shared/ActionSection/ActionSection.tsx
@@ -13,13 +13,13 @@ const ActionSection = ({classing = ""}: ActionSectionI) => {
       {activeTab == 1 &&
       <div className={`ActionSection ${classing}`}>
           <h3>Эксперты Школы BRESSEL владеют уникальными знаниями для расчета Матрицы года и если Вы хотите узнать, что принесёт лично Вам текущий или предстоящий год, напишите нам!</h3>
-          <button>Заказать</button>
+          <a href={"https://t.me/Kris_bresselbot"} target={"_blank"}>Заказать</a>
       </div>
       }
       {activeTab == 2 &&
       <div className={`ActionSection ${classing}`}>
           <h3>Для получения подробного описания Вашей матрицы судьбы обратитесь к эксперту Школы BRESSEL </h3>
-          <button>Запись на консультацию</button>
+          <a href={"https://t.me/Kris_bresselbot"} target={"_blank"}>Запись на консультацию</a>
       </div>
       }
     </>
