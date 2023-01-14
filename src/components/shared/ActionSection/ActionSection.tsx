66d6d17e6ed5a29d31c77f3ec924f05234a69166
sergega-fan <sergei_nikitin00@mail.ru>
2023-01-14 23:22:58 +0300

    done

--- src/components/shared/ActionSection/ActionSection.tsx
+++ src/components/shared/ActionSection/ActionSection.tsx
@@ -10,13 +10,13 @@ const ActionSection = ({classing = ""}: ActionSectionI) => {
 
   return (
     <>
-      {activeTab == 1 &&
+      {activeTab == 2 &&
       <div className={`ActionSection ${classing}`}>
           <h3>Эксперты Школы BRESSEL владеют уникальными знаниями для расчета Матрицы года и если Вы хотите узнать, что принесёт лично Вам текущий или предстоящий год, напишите нам!</h3>
           <a href={"https://t.me/Kris_bresselbot"} target={"_blank"}>Заказать</a>
       </div>
       }
-      {activeTab == 2 &&
+      {activeTab == 1 &&
       <div className={`ActionSection ${classing}`}>
           <h3>Для получения подробного описания Вашей матрицы судьбы обратитесь к эксперту Школы BRESSEL </h3>
           <a href={"https://t.me/Kris_bresselbot"} target={"_blank"}>Запись на консультацию</a>
