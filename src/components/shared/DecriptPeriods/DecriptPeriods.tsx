8d9d7778c70afaf9ec40607a90e9980e7b8a6f94
sergega-fan <sergei_nikitin00@mail.ru>
2023-03-18 14:05:17 +0300

    Try test it

--- src/components/shared/DecriptPeriods/DecriptPeriods.tsx
+++ src/components/shared/DecriptPeriods/DecriptPeriods.tsx
@@ -1,39 +1,43 @@
-import React, {useEffect, useState} from 'react';
-import {useAppSelector} from "../../../hooks/redux";
-import ResultItemList, {numbType} from "../ResultSection/components/ResultItemsList/ResultItemList";
-import decryptPeriods from "../../../data/decryptPeriods";
-import decryptFinance from "../../../data/decryptFinance";
-import decryptComphort from "../../../data/decriptComphort";
+import React, { useEffect, useState } from 'react'
+import { useAppSelector } from '../../../hooks/redux'
+import ResultItemList, { numbType } from '../ResultSection/components/ResultItemsList/ResultItemList'
+import decryptPeriods from '../../../data/decryptPeriods'
+import decryptFinance from '../../../data/decryptFinance'
+import decryptComphort from '../../../data/decriptComphort'
 
 interface DecriptPeriodsI {
-  classing?: string;
-  periods?: number[]
+	classing?: string
+	periods?: number[]
 }
 
-const DecriptPeriods = ({classing = "", periods = [1]}: DecriptPeriodsI) => {
-  const matrixData = useAppSelector(state => state.app.matrixData)
-  const activeTab = useAppSelector(state => state.app.activeTab)
+const DecriptPeriods = ({ classing = '', periods }: DecriptPeriodsI) => {
+	const matrixData = useAppSelector(state => state.app.matrixData)
+	const activeTab = useAppSelector(state => state.app.activeTab)
 
-  const [periodsResult, setPeriodsResult] = useState<numbType>();
-  const [financeResult, setFinanceResult] = useState<numbType>();
-  const [comfortResult, setComfortResult] = useState<numbType>();
+	const [periodsResult, setPeriodsResult] = useState<numbType>()
+	const [financeResult, setFinanceResult] = useState<numbType>()
+	const [comfortResult, setComfortResult] = useState<numbType>()
 
-  useEffect(() => {
-    // @ts-ignore
-    setPeriodsResult([matrixData.years])
-    // @ts-ignore
-    setFinanceResult([matrixData.c2])
-    // @ts-ignore
-    setComfortResult([matrixData.e])
-  }, [matrixData])
+	useEffect(() => {
+		// @ts-ignore
+		setPeriodsResult([matrixData?.years])
+		console.log('period dot', matrixData?.years)
 
-  return (
-    <div className={`${classing} DecryptPeriods`}>
-      {activeTab == 1 && <ResultItemList titleText={"Рекомендации на ближайший период жизни"} texts={decryptPeriods[periodsResult || "1"]} />}
-      {activeTab == 1 && <ResultItemList titleText={"Финансовая карма"} texts={decryptFinance[financeResult || "1"]} />}
-      {activeTab == 2 && <ResultItemList titleText={"Зона комфорта пары"} texts={decryptComphort[comfortResult || "1"]} />}
-    </div>
-  );
-};
+		// @ts-ignore
+		setFinanceResult([matrixData.c2])
+		// @ts-ignore
+		setComfortResult([matrixData.e])
+	}, [matrixData])
 
-export default DecriptPeriods;
+	return (
+		<div className={`${classing} DecryptPeriods`}>
+			{periodsResult}
+
+			{activeTab == 1 && <ResultItemList titleText={'Рекомендации на ближайший период жизни'} texts={decryptPeriods[periodsResult || '1']} />}
+			{activeTab == 1 && <ResultItemList titleText={'Финансовая карма'} texts={decryptFinance[financeResult || '1']} />}
+			{activeTab == 2 && <ResultItemList titleText={'Зона комфорта пары'} texts={decryptComphort[comfortResult || '1']} />}
+		</div>
+	)
+}
+
+export default DecriptPeriods
