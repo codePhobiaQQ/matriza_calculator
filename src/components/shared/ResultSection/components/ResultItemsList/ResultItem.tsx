917615d13864a40e3e32bbe74e198611384d79bf
sergega-fan <sergei_nikitin00@mail.ru>
2023-03-18 15:50:35 +0300

    Done energy number

--- src/components/shared/ResultSection/components/ResultItemsList/ResultItem.tsx
+++ src/components/shared/ResultSection/components/ResultItemsList/ResultItem.tsx
@@ -1,36 +1,33 @@
-import React, { useState } from "react";
-import ArrowDown from "../../../../common/svg/ArrowDown";
-import dataText from "./data";
+import React, { useState } from 'react'
+import ArrowDown from '../../../../common/svg/ArrowDown'
+import dataText from './data'
 
 interface ResultItemI {
-  item: string[];
-  titleText?: string;
+	item: string[]
+	titleText?: string
+	number?: number
 }
 
-const ResultItem = ({ item, titleText }: ResultItemI) => {
-  const [isTabOpen, setIsTabOpen] = useState<boolean>(false);
-  const editOpenHandler = () => {
-    setIsTabOpen((prevState) => !prevState);
-  };
+const ResultItem = ({ item, titleText, number }: ResultItemI) => {
+	const [isTabOpen, setIsTabOpen] = useState<boolean>(false)
+	const editOpenHandler = () => {
+		setIsTabOpen(prevState => !prevState)
+	}
 
-  return (
-    <div
-      onClick={editOpenHandler}
-      className={`ResultItem ${isTabOpen ? "active" : ""}`}
-    >
-      <div className="ResultItemHeader">
-        <span>{titleText}</span>
-        <ArrowDown isActive={isTabOpen} clickHandler={editOpenHandler} />
-      </div>
-      <div
-        className="ResultItemContent"
-      >
-        {item.map((text, index) => (
-          <p key={"text" + index}>{text}</p>
-        ))}
-      </div>
-    </div>
-  );
-};
+	return (
+		<div onClick={editOpenHandler} className={`ResultItem ${isTabOpen ? 'active' : ''}`}>
+			<div className='ResultItemHeader'>
+				<span>{titleText}</span>
+				<ArrowDown isActive={isTabOpen} clickHandler={editOpenHandler} />
+			</div>
+			<div className='ResultItemContent'>
+				{number ? <p>{number} энергия</p> : null}
+				{item.map((text, index) => (
+					<p key={'text' + index}>{text}</p>
+				))}
+			</div>
+		</div>
+	)
+}
 
-export default ResultItem;
+export default ResultItem
--- src/components/shared/ResultSection/components/ResultItemsList/ResultItemList.tsx
+++ src/components/shared/ResultSection/components/ResultItemsList/ResultItemList.tsx
@@ -1,46 +1,22 @@
-import React from "react";
-import decryptFinance from "../../../../../data/decryptFinance";
-import ResultItem from "./ResultItem";
+import React from 'react'
+import decryptFinance from '../../../../../data/decryptFinance'
+import ResultItem from './ResultItem'
 
-export type numbType =
-  1
-  | 2
-  | 3
-  | 4
-  | 5
-  | 6
-  | 7
-  | 8
-  | 9
-  | 10
-  | 11
-  | 12
-  | 13
-  | 14
-  | 15
-  | 16
-  | 17
-  | 18
-  | 19
-  | 20
-  | 21
-  | 22
+export type numbType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22
 
 interface ResultItemListI {
-  isFinance?: boolean;
-  titleText?: string;
-  texts: string[]
+	isFinance?: boolean
+	titleText?: string
+	texts: string[]
+	number?: number
 }
 
-const ResultItemList = ({texts= [], isFinance = false, titleText = ""}: ResultItemListI) => {
-  return (
-    <div className={"ResultItemsList"}>
-      <ResultItem
-        titleText={titleText}
-        item={texts}
-      />
-    </div>
-  );
-};
+const ResultItemList = ({ texts = [], isFinance = false, titleText = '', number }: ResultItemListI) => {
+	return (
+		<div className={'ResultItemsList'}>
+			<ResultItem number={number} titleText={titleText} item={texts} />
+		</div>
+	)
+}
 
-export default ResultItemList;
+export default ResultItemList
